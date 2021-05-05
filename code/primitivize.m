e4d9abd530741bbccf23578531b73975fce3ce2d
kgravel <kgravel@mit.edu>
2021-05-05 16:40:12 -0400

    added primitivize

---
+++ code/primitivize.m
@@ -0,0 +1,11 @@
+function Primitivize(sigma)
+	S := Parent(sigma[1]);
+	G := sub< S | sigma>;
+	partition := MinimalPartition(G);
+	if IsPrimitive(G) then
+		return sigma;
+	end if;
+	G_prim := BlocksImage(G,partition); //does the thing but better
+	assert #GeneratorsSequence(G_prim) eq 3;
+	return GeneratorsSequence(G_prim);
+end function;
