9a31408dd9031a14213a4917a1571ceddd5d0834
Sam Schiavone <sschiavo@mit.edu>
2021-05-17 16:25:32 -0400

    move IsPrimitive

--- code/primitivize.m
+++ code/primitivize.m
@@ -1,10 +1,10 @@
 function Primitivize(sigma)
   S := Parent(sigma[1]);
   G := sub< S | sigma>;
-  partition := MinimalPartition(G);
   if IsPrimitive(G) then
     return sigma;
   end if;
+  partition := MinimalPartition(G);
   G_prim := BlocksImage(G,partition); //does the thing but better
   assert #GeneratorsSequence(G_prim) eq 3;
   return GeneratorsSequence(G_prim);
